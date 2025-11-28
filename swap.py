784c28dfc003340ef51075a2f314e5a804d33d7e
Soumya <01FE24ICA013@gmail.com>
2025-11-28 13:05:04 +0530

    swapping of num

---
+++ swap.py
@@ -0,0 +1,15 @@
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+# Displaying numbers before swapping
+print("\nBefore swapping:")
+print("First number =", num1)
+print("Second number =", num2)
+
+# Swapping the values
+num1, num2 = num2, num1
+
+# Displaying numbers after swapping
+print("\nAfter swapping:")
+print("First number =", num1)
+print("Second number =", num2)
